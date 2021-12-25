8c2560333f18f5384b801dd126481034ca8f58bf
Ubuntu <ubuntu@ip-172-31-4-118.us-east-2.compute.internal>
2021-12-25 12:49:50 +0000

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  rot 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
-  root 'application#hello'  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
 end
